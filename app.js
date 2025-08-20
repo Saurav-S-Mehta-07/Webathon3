c47632d811e14ea8b342fa473db528e8da435955
Saurav s Mehta <sauravmehta631@gmail.com>
2025-08-20 15:01:52 +0530

    updated

---
+++ app.js
@@ -0,0 +1,44 @@
+let body = document.querySelector("body");
+
+
+let bar = document.createElement("button");
+bar.innerHTML = "&#9776";
+bar.classList.add("bar");
+
+let links = document.querySelector(".links");
+links.append(bar);
+
+let barDiv = document.createElement("div");
+let homeA = document.createElement("a");
+let eventA = document.createElement("a");
+let contactA = document.createElement("a");
+
+barDiv.append(homeA);
+barDiv.append(eventA);
+barDiv.append(contactA);
+
+homeA.innerText = "Home";
+eventA.innerText = "Event";
+contactA.innerText = "Contact";
+homeA.setAttribute("href","#home");
+eventA.setAttribute("href","event.html");
+contactA.setAttribute("href","#contact");
+
+bar.addEventListener("click",function(event){
+   event.stopPropagation();
+   barDiv.classList.toggle("barDiv");
+   barDiv.classList.remove("displayNone");
+   if(barDiv.getAttribute("class") =="barDiv"){
+     document.querySelector(".header").insertAdjacentElement("afterend",barDiv);
+   }else{
+     barDiv.remove();
+   }
+
+})
+
+body.addEventListener("click",function(){
+     barDiv.classList.remove("barDiv");
+     barDiv.classList.add("displayNone");
+})
+
+
